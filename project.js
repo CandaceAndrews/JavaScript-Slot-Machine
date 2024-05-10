39dcc5e429c380dfcabba9940590ca65af160994
CandaceAndrews <candacewolfandrews@hotmail.com>
2024-05-09 21:49:23 -0400

    finished

--- project.js
+++ project.js
@@ -119,7 +119,7 @@ const getWinnings = (rows, bet, lines) => {
     let winnings = 0;
 
     for (let row = 0; row < lines; row++) {
-        const symbols = row[row];
+        const symbols = rows[row];
         let allSame = true;
 
         for (const symbol of symbols) {
@@ -135,3 +135,30 @@ const getWinnings = (rows, bet, lines) => {
     }
     return winnings;
 };
+
+const game = () => {
+    let balance = deposit();
+
+    while(true) {
+        console.log("You have a balance of $" + balance);
+        const numberOfLines = getNumberOfLines();
+        const bet = getBet(balance, numberOfLines);
+        balance -= bet * numberOfLines;
+        const reels = spin();
+        const rows = transpose(reels);
+        printRows(rows);
+        const winnings = getWinnings(rows, bet, numberOfLines);
+        balance += winnings;
+        console.log("You won, $" + winnings.toString());
+
+        if (balance <= 0) {
+            console.log("You ran out of money!");
+            break;
+        }
+        const playAgain = prompt("Do you want to play again? (y/n) ");
+
+        if (playAgain != "y") break;
+    }
+};
+
+game();
